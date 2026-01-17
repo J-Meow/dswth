e1d623f4ef0a6ae697dc52a31794023e176456a1
Jmeow <j@jmeow.net>
2026-01-16 21:11:29 -0600

    Start program with config path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let config_dir_location = match env::var("DSWTH_CONFIG_PATH") {
+        Ok(val) => Path::new(&val).to_path_buf(),
+        Err(_e) => {
+            if let Some(config_dir) = dirs::config_dir() {
+                let mut config_dir_final = config_dir;
+                config_dir_final.push("dswth");
+                config_dir_final
+            } else {
+                panic!("No configuration directory found. Please specify the environment variable DSWTH_CONFIG_PATH.");
+            }
+        }
+    };
+    println!("Starting DSWTH with configuration directory: {}", config_dir_location.display());
 }
