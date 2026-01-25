034d482a2afbf0c2a159c626a05c9d820ca7f92b
Jmeow <j@jmeow.net>
2026-01-25 00:08:33 -0600

    Get config dir from env variable and fall back to os folder

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import envPaths from "env-paths"
+
+const paths = envPaths("dswth", { suffix: "" })
+
+const configDir = Bun.env.DSWTH_CONFIG_DIR || paths.config
+console.log("Starting DSWTH with configuration directory: " + configDir)
+
